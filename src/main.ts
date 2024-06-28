39046d55b44fb2752b891628fb0758fe34329abf
SSK <dodieajt@gmail.com>
2024-06-28 16:10:53 +0530

    iuhewi

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,17 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
+  app.enableCors({
+    allowedHeaders: "*",
+    origin: "*"
+  });
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
   }));
