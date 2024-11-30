0fc0ae39b5dcaa60f29e3f11837497efdf5598e6
SSK <dodieajt@gmail.com>
2024-11-30 12:46:53 +0530

    fixxes

--- src/app.service.ts
+++ src/app.service.ts
@@ -62,14 +62,14 @@ export class AppService implements OnModuleInit {
         this.promoteClientService.checkPromoteClients()
       })
 
-      schedule.scheduleJob('test3', ' 25 0 * * * ', 'Asia/Kolkata', async () => {
+      schedule.scheduleJob('test3', ' 50 12 * * * ', 'Asia/Kolkata', async () => {
         const now = new Date();
-        if (now.getUTCDate() % 7 === 0) {
+        if (now.getUTCDate() % 7 === 2) {
           await fetchWithTimeout(`${(ppplbot())}&text=Resetting Banned Channels`)
           setTimeout(async () => {
-            // await this.activeChannelsService.resetAvailableMsgs();
-            // await this.activeChannelsService.updateBannedChannels();
-            // await this.activeChannelsService.updateDefaultReactions();
+            await this.activeChannelsService.resetAvailableMsgs();
+            await this.activeChannelsService.updateBannedChannels();
+            await this.activeChannelsService.updateDefaultReactions();
           }, 30000);
         }
 
@@ -652,7 +652,7 @@ export class AppService implements OnModuleInit {
   }
 
   async findOne(transactionId: string): Promise<Transaction> {
-    const transaction = await this.transactionModel.findOne({ _id:transactionId }).exec();
+    const transaction = await this.transactionModel.findOne({ _id: transactionId }).exec();
 
     if (!transaction) {
       throw new NotFoundException(`Transaction with ID ${transactionId} not found`);
