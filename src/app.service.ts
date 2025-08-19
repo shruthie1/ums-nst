a0da4f77f4bc68552595729e321e3bc5dac58a4d
ssk <dodieajt@gmail.com>
2025-08-19 16:59:29 +0530

    yfiy

--- src/app.service.ts
+++ src/app.service.ts
@@ -88,7 +88,7 @@ export class AppService implements OnModuleInit, OnModuleDestroy {
 
       schedule.scheduleJob('test3', '0 * * * * ', 'Asia/Kolkata', async () => {
         try {
-          const res = await this.userDataService.removeOlderThanOneMonth();
+          const res = await this.userDataService.removeRedundantData();
           console.log("Deleted userdata older than month | count: ", res.deletedCount)
         } catch (e) {
           console.error("Error Deleteing old userData", e)
